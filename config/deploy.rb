9b5e2b000e98a6309e049780f8b6d9be31844e6d
AdnanFurkanAKTEMUR <adnanfurkanaktemur@gmail.com>
2022-07-29 22:54:11 +0300

    linked2

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 # append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs) + %w{public/system}
+append :linked_dirs, %w{"log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "public/uploads", "vendor", "storage}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
